efd2b8c3e46563013eed51c0218ac486bc2ef829
Jankat Yaşar <jankat@sabanciuniv.edu>
2020-11-16 16:03:17 +0300

    CS 300

--- hw1-Memory Usage Simulation/cs300th1/my_heap.cpp
+++ hw1-Memory Usage Simulation/cs300th1/my_heap.cpp
@@ -26,7 +26,7 @@ My_heap::~My_heap()
 		delete heap_begin;
 		heap_begin = ptr;
 	}
-	cout << "At destruction, the heap had a memory leak of " << leak << " bytes." << endl;
+	cout << "At destruction, the heap had a memory leak of " << leak << " bytes" << endl;
 }
 
 memory_block*My_heap::bump_allocate(int num_bytes)
@@ -86,14 +86,14 @@ memory_block*My_heap::first_fit_allocate(int num_bytes)
 		{
 			memory_block* ptr = heap_begin;
 			bool found = false; // going to be true if we find a suitable block
-			while (!(ptr->used == false && ptr->size >= num_bytes))
+			while (ptr != nullptr)
 			{
-				ptr=ptr->right;
 				if (ptr->used == false && ptr->size >= num_bytes)
 				{
 					found = true;
 					break; // we break because  this function allocates the first found block.
 				}
+				ptr = ptr->right;
 			}
 			if (found == true) // if we found it
 			{
@@ -120,9 +120,8 @@ memory_block*My_heap::best_fit_allocate(int num_bytes)
 		memory_block*tobeadded;
 		int diff = -1; // -1 is the default value for now.
 		tracker = heap_begin;
-		while (!(tracker->used == false && tracker->size >= num_bytes))
+		while (tracker != nullptr)
 		{
-			tracker = tracker->right;
 			if (tracker->used == false && tracker->size >= num_bytes)
 			{
 				if (diff == -1 || tracker->size-num_bytes < diff) // if we check for the first time (-1 default value) or if the difference is less its better fit.
@@ -131,6 +130,7 @@ memory_block*My_heap::best_fit_allocate(int num_bytes)
 					diff = tobeadded->size - num_bytes; 
 				}
 			}
+			tracker = tracker->right;
 		}
 		if (diff == -1) // which means there was no suitable memory block.
 		{
@@ -154,14 +154,14 @@ memory_block*My_heap::first_fit_split_allocate(int num_bytes)
 	{
 		memory_block * ptr = heap_begin;
 		bool found = false; // going to be true if we find a suitable block
-		while (!(ptr->used == false && ptr->size >= num_bytes))
+		while (ptr != nullptr)
 		{
-			ptr=ptr->right;
 			if (ptr->used == false && ptr->size >= num_bytes)
 			{
 				found = true;
 				break; // we break because  this function allocates the first found block.
 			}
+			ptr=ptr->right;
 		}
 		if (found == true) // if we found it
 		{
@@ -173,6 +173,7 @@ memory_block*My_heap::first_fit_split_allocate(int num_bytes)
 			else
 			{
 				memory_block * newfreeblk = new memory_block();
+				newfreeblk->size = ptr->size - num_bytes;
 				if (ptr->right != nullptr) // if the ptr is the blk.
 				{
 					newfreeblk->right = ptr->right;
@@ -186,7 +187,7 @@ memory_block*My_heap::first_fit_split_allocate(int num_bytes)
 				newfreeblk->left = ptr;
 				ptr->finishing_adress = ptr->finishing_adress - (ptr->size - num_bytes);
 				newfreeblk->starting_address = ptr->finishing_adress;
-				newfreeblk->finishing_adress = newfreeblk->starting_address + (ptr->size - num_bytes);
+				newfreeblk->finishing_adress = newfreeblk->starting_address + newfreeblk->size;
 				newfreeblk->used = false;
 				ptr->size = num_bytes;
 				ptr->used = true;
@@ -202,7 +203,54 @@ memory_block*My_heap::first_fit_split_allocate(int num_bytes)
 void My_heap::deallocate(memory_block* block_address)
 {
 	used_bytes = used_bytes - block_address->size;
-	if (block_address->left->used == false && block_address->right == false) // in case if block is in between two used=false blocks.
+	if (block_address->left == nullptr && block_address ->right == nullptr)
+	{
+		block_address->used = false; // if its the only block
+	}
+	else if (block_address == heap_begin)
+	{		
+		if (block_address->right->used == false) // in case the right block is a free block.
+		{	
+			memory_block * rightptr = block_address->right;
+			block_address->size += rightptr->size;
+			block_address->finishing_adress = rightptr->finishing_adress; // initialize finishing adress of right because it comes last when we merge.
+			if (rightptr->right != nullptr)  // making right connections before combining.
+			{			
+				block_address->right->right->left = block_address;
+				block_address->right = block_address->right->right;
+			}
+			else
+			{
+				block_address->right = nullptr;
+				blk = block_address; // since there is no block at right, it makes it the blk.
+			}
+			delete rightptr;
+			
+		}
+		block_address->used = false;
+	}
+	else if (block_address == blk)
+	{
+		memory_block * leftptr = block_address->left;
+		if (block_address->left->used == false) // in case the left block is a free block.
+		{			
+			block_address->size += leftptr->size;
+			block_address->starting_address = leftptr->starting_address; // initialize starting adress of left because it comes first when we merge.
+			if (leftptr->left != nullptr) 
+			{
+				leftptr->left->right = block_address; // connecting the left pointer before combining 
+				block_address->left = leftptr->left;			
+			}
+			else
+			{
+				block_address->left = nullptr;
+				heap_begin = block_address; // since there is no block at left, it makes this the heap_begin.
+			}
+			delete leftptr;			
+		}
+		block_address->used = false;
+	}
+	else if (block_address->left->used == false && block_address->right == false) // in case if block is in between two used=false blocks.
 	{
 		memory_block * leftptr = block_address->left;
 		memory_block * rightptr = block_address->right;
@@ -313,7 +361,7 @@ void My_heap::print_heap()
 			usedd = "False";
 		}
 		
-		cout << "Block " << counter <<"\t\tUsed: " << usedd << "\tSize (B);" << tracker->size << "\tStarting Adress: 0x"<<
+		cout << "Block " << counter <<"\t\tUsed: " << usedd << "\tSize (B): " << tracker->size << "\tStarting Adress: 0x"<<
 			hex << tracker->starting_address << dec << endl;
 		tracker = tracker->right;
 		counter++;
--- hw1-Memory Usage Simulation/cs300th1/mymain.cpp
+++ hw1-Memory Usage Simulation/cs300th1/mymain.cpp
@@ -1,15 +1,84 @@
+//#include "my_heap.h"
+//int main(){
+//My_heap heap;
+//heap.print_heap();
+//memory_block* block = heap.bump_allocate(8);
+//memory_block* block1 = heap.bump_allocate(12);
+//memory_block* block2 = heap.bump_allocate(9);
+//memory_block* block3 = heap.bump_allocate(23);
+//heap.print_heap();
+//heap.deallocate(block1);
+//heap.print_heap();
+//heap.deallocate(block2);
+//heap.print_heap();
+//return 0;
+//}
+
 #include "my_heap.h"
+#include <iostream>
+using namespace std;
+
+
 int main(){
-My_heap heap;
-heap.print_heap();
-memory_block* block = heap.bump_allocate(8);
-memory_block* block1 = heap.bump_allocate(12);
-memory_block* block2 = heap.bump_allocate(9);
-memory_block* block3 = heap.bump_allocate(23);
-heap.print_heap();
-heap.deallocate(block1);
-heap.print_heap();
-heap.deallocate(block2);
-heap.print_heap();
-return 0;
+    My_heap heap;
+
+    memory_block* b1 = heap.bump_allocate(10);
+    cout << "~~~~~~~~~~~~~~\n";
+    cout << "Bump allocate 10\n";
+    cout << "~~~~~~~~~~~~~~\n";
+    memory_block* b2 = heap.first_fit_allocate(20);
+    cout << "~~~~~~~~~~~~~~\n";
+    cout << "First fit allocate 20\n";
+    cout << "~~~~~~~~~~~~~~\n";
+    memory_block* b3 = heap.first_fit_split_allocate(10);
+    cout << "~~~~~~~~~~~~~~\n";
+    cout << "First fit split allocate 10\n";
+    cout << "~~~~~~~~~~~~~~\n";
+    memory_block* b4 = heap.best_fit_allocate(15);
+    cout << "~~~~~~~~~~~~~~\n";
+    cout << "Best fit allocate 15\n";
+    cout << "~~~~~~~~~~~~~~\n";
+    heap.print_heap();
+
+    heap.deallocate(b4);
+    cout << "~~~~~~~~~~~~~~\n";
+    cout << "deallocate\n";
+    cout << "~~~~~~~~~~~~~~\n";
+    heap.print_heap();
+    heap.deallocate(b2);
+    cout << "~~~~~~~~~~~~~~\n";
+    cout << "deallocate\n";
+    cout << "~~~~~~~~~~~~~~\n";
+    heap.print_heap();
+
+    memory_block* b5 = heap.best_fit_allocate(12);
+    cout << "~~~~~~~~~~~~~~\n";
+    cout << "Best fit allocate 12\n";
+    cout << "~~~~~~~~~~~~~~\n";
+    heap.print_heap();
+
+    memory_block* b6 = heap.first_fit_split_allocate(16);
+    cout << "~~~~~~~~~~~~~~\n";
+    cout << "First fit split allocate 16\n";
+    cout << "~~~~~~~~~~~~~~\n";
+    heap.print_heap();
+
+    heap.deallocate(b3);
+    cout << "~~~~~~~~~~~~~~\n";
+    cout << "deallocate\n";
+    cout << "~~~~~~~~~~~~~~\n";
+    heap.print_heap();
+    heap.deallocate(b5);
+    cout << "~~~~~~~~~~~~~~\n";
+    cout << "deallocate\n";
+    cout << "~~~~~~~~~~~~~~\n";
+    heap.print_heap();
+    memory_block* b7 = heap.first_fit_allocate(16);
+    cout << "~~~~~~~~~~~~~~\n";
+    cout << "First fit allocate 16\n";
+    cout << "~~~~~~~~~~~~~~\n";
+    heap.print_heap();
+
+    
+    return 0;
 }
